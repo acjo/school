8a960bd0fe9a4d131651db2c03e2e6c5421c78bb
Caelan Osman <caelan@coreform.com>
2021-05-17 11:08:13 -0600

    added command line arguments

--- CS_235/lab01_grades/grades.cpp
+++ CS_235/lab01_grades/grades.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 #ifdef _MSC_VER
-#define _CRTDBG_MAP_ALLOC  
+#define _CRTDBG_MAP_ALLOC
 #include <crtdbg.h>
 #define VS_MEM_CHECK _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #else
@@ -27,16 +27,23 @@ int main(int argc, char* argv[])
     int numStudents;
     int numExams;
 
-    cout << "Enter the path to the input file and press enter: ";
-    getline(cin, inputDataFile);
-    cout << "Enter the path to the output file and press enter: ";
-    getline(cin, outputDataFile);
+    if (argc != 3)
+    {
+	    cout << "incorrect number of arguments" << endl;
+	    return 9;
+    }
+
+    inputDataFile = argv[1];
+    cout << "Input data file: " << inputDataFile << endl;
+
+    outputDataFile = argv[2];
+    cout << "Output data file: " << outputDataFile << endl;
 
     dataInput.open(inputDataFile);
     if (!dataInput.is_open())
     {
         cout << "Unable to open file: " << inputDataFile << endl;
-        return 0;
+        return 9;
     }
 
     dataInput >> numStudents;
@@ -47,17 +54,17 @@ int main(int argc, char* argv[])
     string** examLetterGrades = new string* [numStudents];
     for (unsigned int i = 0; i < numStudents; ++i)
     {
-        examScores[i] = new double[numExams];
-        examLetterGrades[i] = new string[numExams];
+	    examScores[i] = new double[numExams];
+	    examLetterGrades[i] = new string[numExams];
     }
 
     for (unsigned int i = 0; i < numStudents; ++i)
     {
-        dataInput >> firstName;
-        dataInput >> lastName;
-        studentNames[i] = firstName + " " + lastName;
-        for (unsigned int j = 0; j < numExams; ++j)
-            dataInput >> examScores[i][j];
+	    dataInput >> firstName;
+	    dataInput >> lastName;
+	    studentNames[i] = firstName + " " + lastName;
+	    for (unsigned int j = 0; j < numExams; ++j)
+		    dataInput >> examScores[i][j];
     }
     dataInput.close();
 
@@ -65,134 +72,133 @@ int main(int argc, char* argv[])
     dataOutput << "Student Scores:" << endl;
     for (unsigned int i = 0; i < numStudents; ++i)
     {
-        dataOutput << setw(20) << studentNames[i];
-        for (unsigned int j = 0; j < numExams; ++j)
+	    dataOutput << setw(20) << studentNames[i];
+	    for (unsigned int j = 0; j < numExams; ++j)
         {
-            if (j == 0)
-                dataOutput << setw(7) << examScores[i][j];
-            else
-                dataOutput << setw(6) << examScores[i][j];
+		if (j == 0)
+			dataOutput << setw(7) << examScores[i][j];
+		else
+			dataOutput << setw(6) << examScores[i][j];
         }
-        dataOutput << endl;
+	    dataOutput << endl;
     }
 
     dataOutput << "Exam Averages:" << endl;
     for (unsigned int i = 0; i < numExams; ++i)
     {
-        for(unsigned int j = 0; j < numStudents; ++j)
-        {
-            if (j == 0)
-                examAverages[i] = examScores[j][i];
-            else
-                examAverages[i] += examScores[j][i];
-        }
-        examAverages[i] = examAverages[i] / numStudents;
-        dataOutput << fixed << setprecision(1);
-        dataOutput << setw(9) << "Exam " << i + 1 <<  " Average =" << setw(7) << examAverages[i] << endl;
+	    for(unsigned int j = 0; j < numStudents; ++j)
+	    {
+		    if (j == 0)
+			    examAverages[i] = examScores[j][i];
+		    else
+			    examAverages[i] += examScores[j][i];
+	    }
+	    examAverages[i] = examAverages[i] / numStudents;
+	    dataOutput << fixed << setprecision(1);
+	    dataOutput << setw(9) << "Exam " << i + 1 <<  " Average =" << setw(7) << examAverages[i] << endl;
     }
 
     dataOutput <<setprecision(0) << "Student Exam Grades:" << endl;
     for (unsigned int i = 0; i < numStudents; ++i)
     {
-        dataOutput << setw(20) << studentNames[i];
-        for (unsigned int j = 0; j < numExams; ++j)
-        {
-            if (examScores[i][j] + 5 >= examAverages[j] && examScores[i][j] - 5 <= examAverages[j])
-            {
-                examLetterGrades[i][j] = "(C)";
-                dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
-            }
-            else if (examScores[i][j] - 5 > examAverages[j] && examScores[i][j] - 15 < examAverages[j])
-            {
-                examLetterGrades[i][j] = "(B)";
-                dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
+	    dataOutput << setw(20) << studentNames[i];
+	    for (unsigned int j = 0; j < numExams; ++j)
+	    {
+		    if (examScores[i][j] + 5 >= examAverages[j] && examScores[i][j] - 5 <= examAverages[j])
+		    {
+			    examLetterGrades[i][j] = "(C)";
+			    dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
+		    }
+		    else if (examScores[i][j] - 5 > examAverages[j] && examScores[i][j] - 15 < examAverages[j])
+		    {
+			    examLetterGrades[i][j] = "(B)";
+			    dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
 
-            }
-            else if (examScores[i][j] + 5 < examAverages[j] && examScores[i][j] + 15 > examAverages[j])
-            {
-                examLetterGrades[i][j] = "(D)";
-                dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
-            }
-            else if (examScores[i][j] - 15 >= examAverages[j])
-            {
-                examLetterGrades[i][j] = "(A)";
-                dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
-            }
-            else
-            {
-                examLetterGrades[i][j] = "(E)";
-                dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
-            }
-        }
-        dataOutput << endl;
+		    }
+		    else if (examScores[i][j] + 5 < examAverages[j] && examScores[i][j] + 15 > examAverages[j])
+		    {
+			    examLetterGrades[i][j] = "(D)";
+			    dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
+		    }
+		    else if (examScores[i][j] - 15 >= examAverages[j])
+		    {
+			    examLetterGrades[i][j] = "(A)";
+			    dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
+		    }
+		    else
+		    {
+			    examLetterGrades[i][j] = "(E)";
+			    dataOutput << setw(6) << examScores[i][j] << examLetterGrades[i][j];
+		    }
+	    }
+	    dataOutput << endl;
     }
 
     dataOutput << "Exam Grades:" << endl;
     for (unsigned int i = 0; i < numExams; ++i)
     {
-        int aCount = 0;
-        int bCount = 0;
-        int cCount = 0;
-        int dCount = 0;
-        int eCount = 0;
-        for (unsigned int j = 0; j < numStudents; ++j)
-        {
-            if (examLetterGrades[j][i] == "(A)")
-                ++aCount;
-            else if (examLetterGrades[j][i] == "(B)")
-                ++bCount;
-            else if (examLetterGrades[j][i] == "(C)")
-                ++cCount;
-            else if (examLetterGrades[j][i] == "(D)")
-                ++dCount;
-            else
-                ++eCount;
-        }
-        dataOutput << setw(8) << "Exam" << setw(3) << i + 1 << setw(5) << aCount << "(A)" << setw(5) << bCount << "(B)"
-            << setw(5) << cCount << "(C)" << setw(5) << dCount << "(D)" << setw(5) << eCount << "(E)" << endl;
+	    int aCount = 0;
+	    int bCount = 0;
+	    int cCount = 0;
+	    int dCount = 0;
+	    int eCount = 0;
+	    for (unsigned int j = 0; j < numStudents; ++j)
+	    {
+		    if (examLetterGrades[j][i] == "(A)")
+			    ++aCount;
+		    else if (examLetterGrades[j][i] == "(B)")
+			    ++bCount;
+		    else if (examLetterGrades[j][i] == "(C)")
+			    ++cCount;
+		    else if (examLetterGrades[j][i] == "(D)")
+			    ++dCount;
+		    else
+			    ++eCount;
+	    }
+	    dataOutput << setw(8) << "Exam" << setw(3) << i + 1 << setw(5) << aCount << "(A)" << setw(5) << bCount << "(B)"
+		       << setw(5) << cCount << "(C)" << setw(5) << dCount << "(D)" << setw(5) << eCount << "(E)" << endl;
     }
 
     dataOutput << "Student Final Grades:" << endl;
     double classExamAverage = 0;
     for (unsigned int i = 0; i < numStudents; ++i)
-        for (unsigned int j = 0; j < numExams; ++j)
-            classExamAverage += examScores[i][j];
+	    for (unsigned int j = 0; j < numExams; ++j)
+		    classExamAverage += examScores[i][j];
 
     classExamAverage = classExamAverage / (numStudents * numExams);
     for (unsigned int i = 0; i < numStudents; ++i)
     {
-        double studentGrade = 0;
-        dataOutput << setprecision(1) << setw(20) << studentNames[i];
-        for (unsigned int j = 0; j < numExams; ++j)
-            studentGrade += examScores[i][j];
-    
-        studentGrade = studentGrade / numExams;
-        if (studentGrade + 5 >= classExamAverage  && studentGrade - 5 <= classExamAverage)
-            dataOutput << setw(6) << studentGrade << "(C)";
-        else if (studentGrade - 5 > classExamAverage && studentGrade - 15 < classExamAverage)
-            dataOutput << setw(6) << studentGrade << "(B)";
-        else if (studentGrade + 5 < classExamAverage && studentGrade + 15 > classExamAverage)
-            dataOutput << setw(6) << studentGrade << "(D)";
-        else if (studentGrade - 15 >= classExamAverage)
-            dataOutput << setw(6) << studentGrade << "(A)";
-        else
-            dataOutput << setw(6) << studentGrade << "(E)";
-    
-        dataOutput << endl;
+	    double studentGrade = 0;
+	    dataOutput << setprecision(1) << setw(20) << studentNames[i];
+	    for (unsigned int j = 0; j < numExams; ++j)
+		    studentGrade += examScores[i][j];
+
+	    studentGrade = studentGrade / numExams;
+	    if (studentGrade + 5 >= classExamAverage  && studentGrade - 5 <= classExamAverage)
+		    dataOutput << setw(6) << studentGrade << "(C)";
+	    else if (studentGrade - 5 > classExamAverage && studentGrade - 15 < classExamAverage)
+		    dataOutput << setw(6) << studentGrade << "(B)";
+	    else if (studentGrade + 5 < classExamAverage && studentGrade + 15 > classExamAverage)
+		    dataOutput << setw(6) << studentGrade << "(D)";
+	    else if (studentGrade - 15 >= classExamAverage)
+		    dataOutput << setw(6) << studentGrade << "(A)";
+	    else
+		    dataOutput << setw(6) << studentGrade << "(E)";
+
+	    dataOutput << endl;
     }
     dataOutput << "Class Average Score = " << classExamAverage;
     dataOutput.close();
 
     for (int i = 0; i < numStudents; ++i)
     {
-        delete[] examScores[i];
-        delete[] examLetterGrades[i];
+	    delete[] examScores[i];
+	    delete[] examLetterGrades[i];
     }
     delete[] examScores;
     delete[] examLetterGrades;
     delete[] studentNames;
     delete[] examAverages;
-    
-    
+
     return 0;
 }
